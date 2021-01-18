20e9086d93ccfb191eb36a32de46915221dd23fb
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-01-18 15:14:09 +0530

    added api endpoints to test using postman

--- server.js
+++ server.js
@@ -2,8 +2,83 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '1',
+            name: 'John',
+            email: 'john@email.com',
+            password: 'berry',
+            entries: 0,
+            createdAt: new Date()
+        }
+    ]
+}
+
 app.get('/', (req, res, next) => {
-    res.send('hello');
+    res.send(database.users);
+})
+
+app.post('/register', (req, res, next) => {
+
+    const newUser = {
+        id: '2',
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        entries: 0,
+        createdAt: new Date()
+    }
+
+    database.users.push(newUser);
+
+    res.send(newUser);
+})
+
+app.post('/signin', (req, res, next) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.send('success');
+    } else {
+        res.status(400).send('signing failed');
+    }
+})
+
+app.get('/profile/:id', (req, res) => {
+
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
+
+    if (!found) {
+        res.status(400).send('user not found');
+    }
+})
+
+app.post('/image', (req, res) => {
+
+    const { id } = req.body;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+
+    if (!found) {
+        res.status(400).json('user not found');
+    }
+
 })
 
 app.listen(3000, () => {
